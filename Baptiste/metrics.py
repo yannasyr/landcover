cdc82a01b7299d131bb1896264964eedc02caed4
BaptistePodvin <148977052+BaptistePodvin@users.noreply.github.com>
2023-12-27 15:34:23 +0100

    Add files via upload

--- Baptiste/metrics.py
+++ Baptiste/metrics.py
@@ -1,7 +1,8 @@
 import numpy as np
 import torch
-import matplotlib as plt
+import matplotlib.pyplot as plt  # Fix import statement
 from LandCoverData import LandCoverData
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, accuracy_score, jaccard_score
 
 
 
@@ -25,23 +26,48 @@ def mesure_on_batch(batch_gt, batch_predi, batch_size=16):
     mean += klmetric(Y_truth, Y_pred, 10e-8)
   return mean / batch_size
 
-def mesure_on_dataloader(data_loader,device,model):
+def calculate_metrics(y_true, y_pred):
+    precision = precision_score(y_true, y_pred, average='weighted')
+    recall = recall_score(y_true, y_pred, average='weighted')
+    f1 = f1_score(y_true, y_pred, average='weighted')
+    confusion_mat = confusion_matrix(y_true, y_pred)
+    overall_accuracy = accuracy_score(y_true, y_pred)
+    iou_global = jaccard_score(y_true, y_pred, average='weighted')
+    iou_per_class = jaccard_score(y_true, y_pred, average=None)
 
-  mean = 0
-  for i in range(len(data_loader)):
-    test_inputs, test_targets = next(iter(data_loader))
-    test_pixels_values = test_inputs.to(device)
-    model.eval()
-    with torch.no_grad():
-      test_outputs = model(pixel_values=test_pixels_values)
-      test_logits = test_outputs.logits
+    return precision, recall, f1, overall_accuracy, iou_global, iou_per_class
 
-    _, predicted_labels = torch.max(test_logits, dim=1)
 
-    mean += mesure_on_batch(test_targets, predicted_labels)
+def mesure_on_dataloader(data_loader, device, model):
+    mean = 0
+    all_y_true, all_y_pred = [], []
+    
+    for test_inputs, test_targets in data_loader:
+        test_pixels_values = test_inputs.to(device)
+        model.eval()
+        
+        with torch.no_grad():
+            test_outputs = model(pixel_values=test_pixels_values)
+            test_logits = test_outputs.logits
 
-  return mean / len(data_loader)
+        _, predicted_labels = torch.max(test_logits, dim=1)
 
+        mean += mesure_on_batch(test_targets, predicted_labels)
+        
+        # Append the current batch's true and predicted labels
+        all_y_true.extend(test_targets.view(-1).cpu().numpy())
+        all_y_pred.extend(predicted_labels.view(-1).cpu().numpy())
+
+    # Calculate metrics using all batches
+    y_true = np.array(all_y_true)
+    y_pred = np.array(all_y_pred)
+    precision, recall, f1, overall_accuracy, iou_global, iou_per_class = calculate_metrics(y_true, y_pred)
+
+    print(f'Precision: {precision}, Recall: {recall}, F1 Score: {f1}')
+    print(f'Overall Accuracy: {overall_accuracy}, IoU Global: {iou_global}')
+    print('IoU per class:', iou_per_class)
+
+    return mean / len(data_loader)
 
 
 def affichage(model,data_loader,device):
@@ -102,4 +128,6 @@ def affichage(model,data_loader,device):
       plt.title("Predicted Mask")
 
       plt.show()
-      return 
+  
+
+
