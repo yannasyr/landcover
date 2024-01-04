57cc0420f99ee19acd7b4b9fa3127c2e98240c78
BaptistePodvin <148977052+BaptistePodvin@users.noreply.github.com>
2024-01-04 17:45:46 +0100

    Update arg_parser.py

--- Baptiste/arg_parser.py
+++ Baptiste/arg_parser.py
@@ -6,7 +6,6 @@ def parser():
     parser = argparse.ArgumentParser(description='Training')
     parser.add_argument('--segformer',  action='store_true', help='SegFormer for semantic segmantation')
     parser.add_argument('--unet', action='store_true', help='Unet')
-    parser.add_argument('--maskRcnn', action='store_true', help='mask2former')
     parser.add_argument('--classes_to_ignore','-classes_ign', default=[0, 1], nargs='+', type=int, help='List of numbers')
     parser.add_argument('--batch_size', '-batch', default=16, type=int, help='batch_size')    
     
