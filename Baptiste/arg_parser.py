475aed782c159befb2343c05ee23b393e378ca7d
BaptistePodvin <148977052+BaptistePodvin@users.noreply.github.com>
2024-01-04 17:16:35 +0100

    Update arg_parser.py

--- Baptiste/arg_parser.py
+++ Baptiste/arg_parser.py
@@ -8,6 +8,8 @@ def parser():
     # Ajoutez des arguments pour spécifier le modèle
     parser.add_argument('--segformer',  action='store_true', help='SegFormer for semantic segmantation')
     parser.add_argument('--unet', action='store_true', help='Unet')
+    parser.add_argument('--maskRcnn', action='store_true', help='mask2former')
+
     parser.add_argument('--batch_size', '-batch', default=16, type=int, help='batch_size')    
     
     # Analyser les arguments de la ligne de commande
