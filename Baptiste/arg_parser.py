5e9a20b9c1645c02379bbc2ae9da7a6856d009d7
BaptistePodvin <148977052+BaptistePodvin@users.noreply.github.com>
2024-01-09 20:20:53 +0100

    Update arg_parser.py

--- Baptiste/arg_parser.py
+++ Baptiste/arg_parser.py
@@ -8,7 +8,10 @@ def parser():
     parser.add_argument('--unet', action='store_true', help='Unet')
     parser.add_argument('--classes_to_ignore','-classes_ign', default=[0, 1], nargs='+', type=int, help='List of numbers')
     parser.add_argument('--batch_size', '-batch', default=16, type=int, help='batch_size')    
-    parser.add_argument('--save_model','save', action='store_true', help='Saving checkpoints for current model')
+    parser.add_argument('--save_model','-save', action='store_true', help='Saving checkpoints for current model')
+    parser.add_argument('--mit_b4' , action='store_true', help='SegFormer Mit-B4') 
+
+
 
     # Analyser les arguments de la ligne de commande
     args = parser.parse_args()
